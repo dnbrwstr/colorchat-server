7f785bc00a4982910b71970e2e6196d7b52444f0
Daniel Brewster <dnbrwstr@gmail.com>
2015-10-06 13:09:33 -0400

    Annotate PhoneNumberUtils

--- src/lib/PhoneNumberUtils.js
+++ src/lib/PhoneNumberUtils.js
@@ -7,16 +7,20 @@ let numberUtil = PhoneNumberUtil.getInstance();
 export let makeString = n =>
   `+${n.countryCode}${n.destinationCode}${n.baseNumber}`;
 
+// Check if a number is dialable
 export let validate = (number, countryCode) => {
   let region = numberUtil.getRegionCodeForCountryCode(countryCode);
   let parsedNumber = getParsedNumber(number, region);
   return parsedNumber && numberUtil.isValidNumber(parsedNumber);
 };
 
-export let normalize = (numberOrArray, userNumber) => {
+// Transform an arbitrary, user-input number into a
+// dialable number. Will attempt to copy missing region
+// and destination codes from reference number
+export let normalize = (numberOrArray, referenceNumber) => {
   return numberOrArray instanceof Array ?
-    numberOrArray.map(n => normalize(n, userNumber)) :
-    normalizeWithPermutations(numberOrArray, userNumber);
+    numberOrArray.map(n => normalize(n, referenceNumber)) :
+    normalizeWithPermutations(numberOrArray, referenceNumber);
 };
 
 let normalizeWithPermutations = (number, baseNumber) => {
@@ -28,8 +32,8 @@ let normalizeWithPermutations = (number, baseNumber) => {
   return getFirstResult(permutations, (n) => normalizeSingleNumber(n, baseNumber)) || null;
 };
 
-let normalizeSingleNumber = (number, userNumber) => {
-  let baseNumberData = parse(userNumber);
+let normalizeSingleNumber = (number, referenceNumber) => {
+  let baseNumberData = parse(referenceNumber);
   let baseRegion = baseNumberData ? baseNumberData.region : defaultRegion;
   let parsedNumberData = parse(number, baseRegion);
 
@@ -56,8 +60,8 @@ let parse = (numberToParse, region=defaultRegion) => {
   };
 };
 
-// Parse wrapped to simplify use as libphonenumber
-// throws when it's unable to parse
+// Wrapping this to simplify use as libphonenumber
+// will throw an error when it's unable to parse
 let getParsedNumber = (number, region) => {
   try {
     return numberUtil.parse(number, region);
@@ -66,12 +70,15 @@ let getParsedNumber = (number, region) => {
   }
 };
 
-let maybeCopyDestinationCode = (numberData, baseData) => {
+// Assuming that the number's destination code
+// is the same as the base number's if it's
+// missing and the regions are consistent
+let maybeCopyDestinationCode = (numberData, referenceData) => {
   if (!numberData.destinationCode &&
-     (!numberData.region || numberData.region == baseData.region)) {
+     (!numberData.region || numberData.region == referenceData.region)) {
     return {
       ...numberData,
-      destinationCode: baseData.destinationCode
+      destinationCode: referenceData.destinationCode
     };
   } else {
     return numberData;
