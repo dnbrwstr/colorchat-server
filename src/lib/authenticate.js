4c34ee945b529b00edc716db456071a4250d34d1
Daniel Brewster <dnbrwstr@gmail.com>
2015-10-06 12:06:20 -0400

    Simplify authenticate function

--- src/lib/authenticate.js
+++ src/lib/authenticate.js
@@ -4,18 +4,13 @@ import User from '../models/User';
 
 let authenticate = wrapAsyncRoute(async function (req, res, next) {
   let token = req.headers['x-auth-token'];
+  if (!token) throw new PermissionsError('User is not logged in');
 
-  if (token) {
-    let user = await User.findByToken(token);
-    if (user) {
-      req.user = user;
-      next();
-    } else {
-      throw new PermissionsError('Invalid session');
-    }
-  } else {
-    throw new PermissionsError('User is not logged in');
-  }
+  let user = await User.findByToken(token);
+  if (!user) throw new PermissionsError('Invalid session');
+
+  req.user = user;
+  next();
 });
 
 export default authenticate;
